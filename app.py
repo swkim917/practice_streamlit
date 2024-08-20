86720e7a9bc71ce718c7039e6de86ef406e358db
swkim917 <39894390+swkim917@users.noreply.github.com>
2024-08-21 01:02:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # text input
-a = int(st.text_input('첫번째 수', placeholder='첫번째 수를 입력하세요'))
-b = int(st.text_input('두번째 수', placeholder='두번째 수를 입력하세요'))
+a = st.text_input('첫번째 수', placeholder='첫번째 수를 입력하세요')
+b = st.text_input('두번째 수', placeholder='두번째 수를 입력하세요')
 
 st.text(str(a + b))
