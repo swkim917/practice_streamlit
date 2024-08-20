b41025378980ebfafaa38878400d7a9dabf3cca7
swkim917 <39894390+swkim917@users.noreply.github.com>
2024-08-21 01:08:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 a = st.text_input('첫번째 수', placeholder='첫번째 수를 입력하세요')
 b = st.text_input('두번째 수', placeholder='두번째 수를 입력하세요')
 
-if a and b :
+if a.isdecimal() and b.isdecimal() :
   a = int(a)
   b = int(b)
   st.text('결과값은 ' + str(a + b) + ' 입니다.')
